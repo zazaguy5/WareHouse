4fff708057bd17e4fc05e9cc7311bcb48d300fc2
Nick-Suthida <nnnnnnsuthida@gmail.com>
2021-04-13 17:38:42 +0700

    แก้ไขครั้งที่ 3

--- UI.py
+++ UI.py
@@ -4,52 +4,100 @@ import mysql.connector
 import Database
 import sys
 
-class Ui_MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.row_table = 12
-        self.setWindowTitle("โปรแกรมจักการคลังสินค้า")
-        self.resize(1024, 600)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        #super().__init__()
+        MainWindow.setObjectName("MainWindow")
+        self.row_table = 100
+        MainWindow.setWindowTitle("โปรแกรมการจัดการคลังสินค้า")
+        MainWindow.resize(1500, 750)
+        MainWindow.setStyleSheet("background-image: url(C:/Users/NICK/Desktop/WareHouse-main/WareHouse-main/resource/background_img.jpg);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
         self.icon = QtGui.QIcon()
         self.icon.addPixmap(QtGui.QPixmap("resource/warehouse_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.setWindowIcon(self.icon)
+        MainWindow.setWindowIcon(self.icon)
 
         #================================================= Input/Output Data ================================================
+        self.title = QLabel(self.centralwidget)
+        self.title.setGeometry(QtCore.QRect(330, 60, 780, 61))
+        self.title.setStyleSheet("font-size: 65px; color: rgb(255,255,255);")
+        self.title.setText("โปรแกรมการจัดการคลังสินค้า")
 
-        self.show_table = QtWidgets.QTableWidget(self)
-        self.show_table.setGeometry(QtCore.QRect(10,10,450,325))
-        self.show_table.setColumnCount(4)
-        self.show_table.setRowCount(self.row_table)
-        self.show_table.setHorizontalHeaderItem(0, QtWidgets.QTableWidgetItem("ID"))
-        self.show_table.setHorizontalHeaderItem(1, QtWidgets.QTableWidgetItem("ชื่อสินค้า"))
-        self.show_table.setHorizontalHeaderItem(2, QtWidgets.QTableWidgetItem("จำนวนสินค้า"))
-        self.show_table.setHorizontalHeaderItem(3, QtWidgets.QTableWidgetItem("จำนวน"))
-        self.show_table.horizontalHeader().setStretchLastSection(True)
-        #self.show_table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        self.show_table1 = QtWidgets.QTableWidget(self.centralwidget)
+        self.show_table1.setGeometry(QtCore.QRect(20,160,541,461))
+        self.show_table1.setColumnCount(4)
+        self.show_table1.setRowCount(self.row_table)
+        self.show_table1.setHorizontalHeaderItem(0, QtWidgets.QTableWidgetItem("ID"))
+        self.show_table1.setHorizontalHeaderItem(1, QtWidgets.QTableWidgetItem("ชื่อสินค้า"))
+        self.show_table1.setHorizontalHeaderItem(2, QtWidgets.QTableWidgetItem("ชนิดสินค้า"))
+        self.show_table1.setHorizontalHeaderItem(3, QtWidgets.QTableWidgetItem("จำนวน"))
+        self.show_table1.horizontalHeader().setStretchLastSection(True)
+        self.show_table1.setStyleSheet(
+            "font: 10pt;"
+            "background-image: url(C:/Users/NICK/Desktop/WareHouse-main/WareHouse-main/resource/bhwzt6.gif);"
+        )
 
-        self.text1 = QLabel(self)
-        self.text1.setGeometry(QtCore.QRect(540,10,50,20))
+        self.show_table2 = QtWidgets.QTableWidget(self.centralwidget)
+        self.show_table2.setGeometry(QtCore.QRect(940,160,541,461))
+        self.show_table2.setColumnCount(4)
+        self.show_table2.setRowCount(self.row_table)
+        self.show_table2.setHorizontalHeaderItem(0, QtWidgets.QTableWidgetItem("ID"))
+        self.show_table2.setHorizontalHeaderItem(1, QtWidgets.QTableWidgetItem("ชื่อสินค้า"))
+        self.show_table2.setHorizontalHeaderItem(2, QtWidgets.QTableWidgetItem("ชนิดสินค้า"))
+        self.show_table2.setHorizontalHeaderItem(3, QtWidgets.QTableWidgetItem("จำนวน"))
+        self.show_table2.horizontalHeader().setStretchLastSection(True)
+        self.show_table2.setStyleSheet(
+            "font: 10pt;"
+            "background-image: url(C:/Users/NICK/Desktop/WareHouse-main/WareHouse-main/resource/bhwzt6.gif);"
+        )
+
+
+        self.text1 = QLabel(self.centralwidget)
+        self.text1.setGeometry(QtCore.QRect(610,220,80,20))
         self.text1.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
         self.text1.setText("ID สินค้า:")
-        self.good_id = QtWidgets.QLineEdit(self)
-        self.good_id.setGeometry(QtCore.QRect(600,10,100,20))
-        self.good_id.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
+        self.good_id = QtWidgets.QLineEdit(self.centralwidget)
+        self.good_id.setGeometry(QtCore.QRect(705,220,191,31))
+        self.good_id.setStyleSheet(
+            "QLineEdit {"
+                "font: 10pt;"
+                "background: rgb(159,226,191);" 
+                "font-family: Ms Shell Dlg 2;"
+            "}"
+        )
 
-        self.text2 = QLabel(self)
-        self.text2.setGeometry(QtCore.QRect(540,50,50,20))
+        self.text2 = QLabel(self.centralwidget)
+        self.text2.setGeometry(QtCore.QRect(610,300,70,20))
         self.text2.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
         self.text2.setText("ชื่อสินค้า:")
-        self.good_name = QtWidgets.QLineEdit(self)
-        self.good_name.setGeometry(QtCore.QRect(600,50,100,20))
-        self.good_name.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
+        self.good_name = QtWidgets.QLineEdit(self.centralwidget)
+        self.good_name.setGeometry(QtCore.QRect(705,300,191,31))
+        self.good_name.setStyleSheet(
+            "QLineEdit {"
+                "font: 10pt;"
+                "background: rgb(159,226,191);" 
+                "font-family: Ms Shell Dlg 2;"
+            "}"
+        )
 
-        self.text3 = QLabel(self)
-        self.text3.setGeometry(QtCore.QRect(540,90,60,20))
+        self.text3 = QLabel(self.centralwidget)
+        self.text3.setGeometry(QtCore.QRect(610,380,100,20))
         self.text3.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
         self.text3.setText("ชนิดสินค้า:")
-        self.good_type = QtWidgets.QComboBox(self)
-        self.good_type.setGeometry(QtCore.QRect(605,90,138,20))
-        self.good_type.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
+        self.good_type = QtWidgets.QComboBox(self.centralwidget)
+        self.good_type.setGeometry(QtCore.QRect(705,380,191,31))
+        self.good_type.setStyleSheet(
+            "QComboBox {" 
+                "color: white;"
+                "background-color: rgb(159,226,191);"
+                "font: 10pt \"Ms Shell Dlg 2\";"
+            "}"
+            "QComboBox::drop-down {"
+                "background-image: url(C:/Users/NICK/Desktop/WareHouse-main/WareHouse-main/resource/drop-down-arrow.png);"
+            "}"
+        )
+        self.good_type.addItem("")
         self.good_type.addItem("เครื่องใช้ไฟฟ้า")
         self.good_type.addItem("เครื่องดื่ม")
         self.good_type.addItem("เครื่องปรุง")
@@ -57,54 +105,58 @@ class Ui_MainWindow(QWidget):
         self.good_type.addItem("ขนมขบเคี้ยว")
         self.good_type.addItem("อุปกรณ์การเรียน")
         self.good_type.addItem("ผลิตภัณฑ์สุขภาพ")
+        self.good_type.setCurrentText("")
 
-        self.text4 = QLabel(self)
-        self.text4.setGeometry(QtCore.QRect(540,130,80,20))
+        self.text4 = QLabel(self.centralwidget)
+        self.text4.setGeometry(QtCore.QRect(610,460,100,20))
         self.text4.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
         self.text4.setText("จำนวนสินค้า:")
-        self.good_amount = QtWidgets.QLineEdit(self)
-        self.good_amount.setGeometry(QtCore.QRect(620,130,100,20))
-        self.good_amount.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
+        self.good_amount = QtWidgets.QLineEdit(self.centralwidget)
+        self.good_amount.setGeometry(QtCore.QRect(705,460,191,31))
+        self.good_amount.setStyleSheet(
+            "QLineEdit {"
+                "font: 10pt;"
+                "background: rgb(159,226,191);" 
+                "font-family: Ms Shell Dlg 2;"
+            "}"
+        )
 
-        self.search = QtWidgets.QLineEdit(self)
-        self.search.setGeometry(QtCore.QRect(500,220,100,20))
-        self.search.setStyleSheet("font: 10pt \"Ms Shell Dlg 2\";")
-        self.search_icon = QPushButton(self)
-        self.search_icon.setGeometry(QtCore.QRect(600,220,20,20))
+        self.text5 = QLabel(self.centralwidget)
+        self.text5.setGeometry(QtCore.QRect(600,150,100,20))
+        self.text5.setStyleSheet(
+            "font: 10pt;"
+            "font-family: Ms Shell Dlg 2;"
+        )
+        self.text5.setText("ค้นหาสินค้า:")
+        self.search = QtWidgets.QLineEdit(self.centralwidget)
+        self.search.setGeometry(QtCore.QRect(690,150,191,31))
+        self.search.setStyleSheet(
+            "font: 10pt \"Ms Shell Dlg 2\";"
+            "background: rgb(159,226,191);"
+        )
+        self.search_icon = QPushButton(self.centralwidget)
+        self.search_icon.setGeometry(QtCore.QRect(880,150,30,31))
         self.search_icon.setStyleSheet(
-            "QPushButton:open { /* when the button has its menu open */"
-                "background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,stop: 0 #dadbde, stop: 1 #f6f7fa);"
-            "}"
-
-            "QPushButton::menu-indicator {"
-                "image: url(menu_indicator.png);"
-                "subcontrol-origin: padding;"
-                "subcontrol-position: bottom right;"
-            "}"
-
-            "QPushButton::menu-indicator:pressed, QPushButton::menu-indicator:open {"
-                "position: relative;"
-                "top: 2px; left: 2px; /* shift the arrow by 2 px */"
-            "}"
+            "background-image: url(C:/Users/NICK/Desktop/WareHouse-main/WareHouse-main/resource/search_icon.png);"
         )
         self.search_icon.clicked.connect(self.search_db)
 
         #================================================ Buttons =========================================================
 
-        self.show_tablebutton = QPushButton(self)
-        self.show_tablebutton.setGeometry(QtCore.QRect(10,500,80,29))
-        self.show_tablebutton.setStyleSheet(
+        self.show_table1button = QPushButton(self.centralwidget)
+        self.show_table1button.setGeometry(QtCore.QRect(10,700,80,29))
+        self.show_table1button.setStyleSheet(
             "font: 10pt \"Ms Shell Dlg 2\";"
             "background-color: rgb(0,100,255);"
             "color: white;"
             "border-style: outset;"
             "border-radius: 6px;"
         )
-        self.show_tablebutton.setText("เปิดตาราง")
-        self.show_tablebutton.clicked.connect(self.show_db)
+        self.show_table1button.setText("เปิดตาราง")
+        self.show_table1button.clicked.connect(self.show_db)
 
-        self.adddata_button = QPushButton(self)
-        self.adddata_button.setGeometry(QtCore.QRect(550,170,80,29))
+        self.adddata_button = QPushButton(self.centralwidget)
+        self.adddata_button.setGeometry(QtCore.QRect(600,520,80,29))
         self.adddata_button.setStyleSheet(
             "font: 10pt \"Ms Shell Dlg 2\";"
             "background-color: rgb(0,100,255);"
@@ -115,8 +167,8 @@ class Ui_MainWindow(QWidget):
         self.adddata_button.setText("เพิ่มข้อมูล")
         self.adddata_button.clicked.connect(self.add_db)
 
-        self.deletedata_button = QPushButton(self)
-        self.deletedata_button.setGeometry(QtCore.QRect(650,170,80,29))
+        self.deletedata_button = QPushButton(self.centralwidget)
+        self.deletedata_button.setGeometry(QtCore.QRect(700,520,80,29))
         self.deletedata_button.setStyleSheet(
             "font: 10pt \"Ms Shell Dlg 2\";"
             "background-color: rgb(0,100,255);"
@@ -127,8 +179,8 @@ class Ui_MainWindow(QWidget):
         self.deletedata_button.setText("ลบข้อมูล")
         self.deletedata_button.clicked.connect(self.delete_db)
 
-        self.clr_button = QPushButton(self)
-        self.clr_button.setGeometry(QtCore.QRect(120,500,80,29))
+        self.clr_button = QPushButton(self.centralwidget)
+        self.clr_button.setGeometry(QtCore.QRect(120,700,80,29))
         self.clr_button.setStyleSheet(
             "font: 10pt \"Ms Shell Dlg 2\";"
             "background-color: rgb(0,100,255);"
@@ -139,8 +191,8 @@ class Ui_MainWindow(QWidget):
         self.clr_button.setText("เคลียร์ข้อมูล")
         self.clr_button.clicked.connect(self.clr_data)
 
-        self.close_main = QPushButton(self)
-        self.close_main.setGeometry(QtCore.QRect(840,500,80,29))
+        self.close_main = QPushButton(self.centralwidget)
+        self.close_main.setGeometry(QtCore.QRect(1240,680,80,29))
         self.close_main.setStyleSheet(
             "font: 10pt \"Ms Shell Dlg 2\";"
             "background-color: rgb(0,100,255);"
@@ -151,17 +203,19 @@ class Ui_MainWindow(QWidget):
         self.close_main.setText("ปิดโปรแกรม")
         self.close_main.clicked.connect(self.close_it)
 
-        self.update_button = QPushButton(self)
-        self.update_button.setGeometry(QtCore.QRect(750,50,80,29))
+        self.update_button = QPushButton(self.centralwidget)
+        self.update_button.setGeometry(QtCore.QRect(800,520,80,29))
         self.update_button.setStyleSheet(
             "font: 9pt \"Ms Shell Dlg 2\";"
-            "background-color: rgb(0,100,255);"
+            "background-color: rgb(255,179,71);"
             "color: white;"
             "border-style: outset;"
-            "border-radius: 6px;"
+            "border-radius: 10px;"
         )
         self.update_button.setText("อัพเดพข้อมูล")
         self.update_button.clicked.connect(self.update_db)
+
+        MainWindow.setCentralWidget(self.centralwidget)
 
     #=================================================== Functions ========================================================
 
@@ -172,9 +226,9 @@ class Ui_MainWindow(QWidget):
         for row in data:
             for j in range(4):
                 if j==0 or j==3:
-                    self.show_table.setItem(row_num,j, QtWidgets.QTableWidgetItem(str(row[j])))
+                    self.show_table1.setItem(row_num,j, QtWidgets.QTableWidgetItem(str(row[j])))
                 else:
-                    self.show_table.setItem(row_num,j, QtWidgets.QTableWidgetItem(row[j]))
+                    self.show_table1.setItem(row_num,j, QtWidgets.QTableWidgetItem(row[j]))
             row_num += 1
 
     def add_db(self):
@@ -187,7 +241,7 @@ class Ui_MainWindow(QWidget):
         Database.delete_data(ID)
         i = int(ID)-1
         for j in range(4):
-            self.show_table.setItem(i,j, QtWidgets.QTableWidgetItem(""))
+            self.show_table1.setItem(i,j, QtWidgets.QTableWidgetItem(""))
 
     def search_db(self):
         ID = self.search.text()
@@ -202,7 +256,7 @@ class Ui_MainWindow(QWidget):
         self.show_db()
 
     def close_it(self):
-        self.close()
+        MainWindow.close()
 
     def clr_data(self):
         self.good_id.setText("")
@@ -211,6 +265,8 @@ class Ui_MainWindow(QWidget):
     
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-    ui.show()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
